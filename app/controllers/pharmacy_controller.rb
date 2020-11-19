b7dd2292c1e8d4bddb58a0bcd6eac3bd12e8df16
torionj <t33zus@gmail.com>
2020-11-19 01:11:43 -0600

    Add commennts to code

--- app/controllers/pharmacy_controller.rb
+++ app/controllers/pharmacy_controller.rb
@@ -2,11 +2,15 @@ require 'csv'
 
 class PharmacyController < ApplicationController
   def show
+    # prepping the latitude & longitude for the closest_pharma function.
     lat_lon_arr = params[:latlon].split(',')
     lat,lon = lat_lon_arr
+    # replacing the semi colons with periods.
     lat[";"] = '.'
     lon[";"] = '.'
 
+    # returns a JSON of the closest pharmacy with properties: name, address, and miles.
+    #  to_f => converts srings to a float.
     closest_pharma = get_closest_pharma([lat.to_f, lon.to_f])
 
     render json: closest_pharma, status: :ok
@@ -14,10 +18,15 @@ class PharmacyController < ApplicationController
 
   private
   def get_closest_pharma(lat_lon_array)
+    # @param lat_lon_array => is an array of floats containing the user's coordinates.
+    # Geocoder::Calculations.distance_between(pointA, pointB) returns the miles between point A and point B.
+    # returns a JSON object with properties => name:string, address:string, miles:float.
     miles = 0
     name, address = "", ""
 
+    # pharmas => is an array of arrays containing each store's information.
     pharmas = CSV.read('app/data/pharmacies.csv')
+    # gets rid of the headers row.
     pharmas.slice!(0)
 
     pharmas.each do |pharmacy|
